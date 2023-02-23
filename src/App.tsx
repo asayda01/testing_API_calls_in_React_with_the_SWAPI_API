8bb29c4bf82ee63718b448191f4472a656b1f4e4
asayda01 <ahmetcemsaydam@gmail.com>
2023-02-23 17:07:40 +0000

    installed axios created character.tsx refactored some files
    + installed axios to handle API calls
    
    - refactored App.test.tsx with removing  some parts of the pre-defined test
    
    + created a Character file  , character.tsx , to use our character interface on this project
    
    + / - refactored App.tsx file , now it uses :
    
    ++ a react hook , useEffect , to react the state of our date in our project
    ++ a try-catch method for our basic API calls and possible errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React , { useEffect , useState } from 'react';
 import './App.css';
+import axios from "axios";
+
+import { interfaceCharacter } from './components/character';
+import { async } from 'q';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-      </header>
-    </div>
+  const [characterApp, setcharacterApp] = useState <Array<interfaceCharacter> | React.ReactNode> ();
+
+  const fetchACharacter = async (id:number) => {
+
+    try {
+
+      const apiResponse = await axios.get(`https://swapi.dev/api/people/${id}`);
+      setcharacterApp(apiResponse.data);
+      console.log(characterApp);
+
+    } catch (error) {
+
+      console.log(" this error is catched : ->->-> "  + error +  " <-<-<- : this error is catched ")
+    }
+
+  }
+
+  useEffect( () => {fetchACharacter(1) },[]);
+
+  console.log(characterApp);
+
+  return (
+
+    <>
+
+      The Selected Character is : {characterApp }
+
+    </>
+        
   );
-}
+
+};
 
 export default App;
